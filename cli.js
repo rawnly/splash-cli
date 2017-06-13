b52f86f45b403b2501024acd7518260a189cb090
Rawnly <fedevitale99@gmail.com>
2017-06-13 23:06:59 +0200

    better organization

--- cli.js
+++ cli.js
@@ -1,62 +1,62 @@
 #!/usr/bin/env node
 
-// Modules
-const path = require('path');
+// MODULES
+const chalk     = require("chalk");
+const clear     = require("clear");
+const Conf      = require("conf");
+const execa     = require("execa");
+const figlet    = require("figlet");
+const frun      = require("first-run");
+const meow      = require("meow");
+const mkdirp    = require("mkdirp");
+const normalize = require("normalize-url");
+const Ora       = require("ora");
+const progress  = require("progress");
+const updateNotifier  = require("update-notifier");
+const wallpaper = require("wallpaper");
+
+// NODE MODULES
 const os = require('os');
-const chalk = require('chalk');
-const meow = require('meow');
-const isOnline = require('is-online');
-const updateNotifier = require('update-notifier');
-const clear = require('clear');
-const Conf = require('conf');
-const firstRun = require('first-run');
-const figlet = require('figlet');
-const normalize = require('normalize-url');
+const fs = require('fs');
+const node_path = require('path');
 
-// Local
-const splash = require('./libs/core');
+// Utilities
+const splash    = require('./libs/core');
+const download  = require("./libs/download");
+const pathParse = require('./libs/pathparser');
+
+// OPTIONS
+const cleanCMD       = require('./options/clean');
+const dirCMD         = require('./options/dir');
+const idCMD          = require('./options/id');
+const listCMD        = require('./options/list');
+const restoreCMD     = require('./options/restore');
+const saveCMD        = require('./options/save');
+const sizeCMD        = require('./options/size');
+const updateCMD      = require('./options/update');
+
+// PACKAGE JSON
 const pkg = require('./package.json');
-const download = require('./libs/download');
-const updateCmd = require('./options/update');
-const restoreCmd = require('./options/restore');
-const dirCmd = require('./options/dir');
-const listCmd = require('./options/list');
-const saveCmd = require('./options/save');
-const idCmd = require('./options/id');
-const cleanCmd = require('./options/clean');
-const sizeCmd = require('./options/size');
 
-// Variables
-const log = console.log;
-const join = path.join;
+// VARIOUS VARIABLES
+const token  = 'daf9025ad4da801e4ef66ab9d7ea7291a0091b16d69f94972d284c71d7188b34';
+const apiURL = normalize(`https://api.unsplash.com/photos/random?client_id=${token}`);
+
+// OBJECTS
+const config = new Conf();
+const spinner = new Ora();
+
+// SHORTCUTS
+const join = node_path.join;
 const home = os.homedir();
 const user = home.split('/')[home.split('/').length - 1];
-const config = new Conf();
 const notifier = updateNotifier({pkg, updateCheckInterval: 1000});
-const token = 'daf9025ad4da801e4ef66ab9d7ea7291a0091b16d69f94972d284c71d7188b34';
-const apiUrl = normalize(`https://api.unsplash.com/photos/random?client_id=${token}`);
 
-// Welcome Message
-if (firstRun()) {
-  clear();
-  config.set('proxy', false);
-  config.set('pic_dir', join(home, 'Pictures', 'splash_photos'));
-  log(`Hello ${chalk.bold(user.toString())}, all photos are stored in ${chalk.yellow.underline(config.get('pic_dir'))}`);
-  log('');
-  log('');
-  log(figlet.textSync('Splash'));
-  log('');
-  log('');
-}
+const cli = new meow(`
+    Usage: ${chalk.yellow('splash')} ${chalk.gray('[sub-command] [flags]')}
 
-// Initializing
-const cli = meow(`
-
-    USAGE: ${chalk.yellow('splash')} ${chalk.gray('[subcommand]  [--flags]')}
-
-
-    ${chalk.yellow('-h --help')}                          ${chalk.gray('# Display this message')}
-    ${chalk.yellow('-v --version')}                       ${chalk.gray('# Display splash version')}
+    ${chalk.yellow('-h --help')}                          ${chalk.gray('Display this message')}
+    ${chalk.yellow('-v --version')}                       ${chalk.gray('Display splash version')}
 
     ${chalk.blue('Picker parameters')}
 
@@ -86,33 +86,28 @@ const cli = meow(`
       ${chalk.yellow.bold('clean')}                       ${chalk.gray('# Delete all downloaded photos.')}
       ${chalk.yellow.bold('restore')}                     ${chalk.gray('# Restore settings to default.')}
 
-
-      ${chalk.yellow('--offline')} ${chalk.gray('<true|false>')}           ${chalk.gray('# Set as true if you are under proxy (Default: false)')}
       ${chalk.yellow('-p --progress')}                    ${chalk.gray('# Show progressbar during downloads (Default: false)')}
-      ${chalk.yellow('-t --theme')}                       ${chalk.gray('# macOS Only! Set the the theme base on color brightness')}`, {
+  `, {
+    alias: {
+    h: 'help',
+    v: 'version',
+    i: 'info',
+    u: 'user',
+    d: 'dest',
+    s: 'set',
+    p: 'progress'
+  }
+});
 
-        alias: {
-          d: 'dest',
-          p: 'progress',
-          t: 'theme',
-          i: 'info',
-          f: 'featured',
-          v: 'version',
-          s: 'set',
-          h: 'help',
-          u: 'user'
-        }
-      });
 
-// Main Function for meow
-function sp(action, flags) {
+function sp(command, flags) {
   if (notifier.update) {
     if (flags.update) {
       updateCmd();
     } else {
       notifier.notify({
         message: `${chalk.dim(notifier.update.current)} -> ${chalk.green(notifier.update.latest)}` +
-        `\n Run ${chalk.cyan('splash --update')} to update`,
+        `\n Run ${chalk.cyan('splash update')} to update`,
         boxenOpts: {
           padding: 1,
           margin: 2,
@@ -122,90 +117,109 @@ function sp(action, flags) {
         }
       });
     }
-  } else if (action === 'restore') {
-      // RESTORE
-    restoreCmd();
-  } else if (action === 'update') {
-      // UPDATE
-    isOnline().then(value => {
-      if (value || config.get('proxy')) {
-        updateCmd();
-      } else {
-        log('I need an internet connection!');
-        process.exit();
-      }
-    });
-  } else if (action === 'clean') {
-    cleanCmd();
-  } else if (action === 'list') {
-    listCmd(flags);
-  } else if (action === 'save') {
-    isOnline().then(value => {
-      if (value || config.get('proxy')) {
-        saveCmd(flags);
-      } else {
-        log('I need an internet connection!');
-        process.exit();
-      }
-    });
-  } else if (flags.id) {
-    // ID
-    isOnline().then(value => {
-      if (value || config.get('proxy')) {
-        idCmd(flags);
-      } else {
-        log('I need an internet connection!');
-        process.exit();
-      }
-    });
-  } else if (flags.size) {
-    isOnline().then(value => {
-      if (value || config.get('proxy')) {
-        sizeCmd(flags);
-      } else {
-        console.log('I need an internet connection!');
-        process.exit();
-      }
-    });
-  } else if (action === 'dir') {
-    // Dir
-    dirCmd(flags);
-  } else if (flags.offline) {
-    config.set('proxy', flags.offline);
-    console.log(`Proxy: ${config.get('proxy')}`);
   } else {
-    // Splash Classic
-    isOnline().then(value => {
-      if (value || config.get('proxy')) {
-        let url = '';
+    if (command) {
 
-        if (flags.user) {
-          url = `${apiUrl}&username=${flags.user}`;
-        } else if (flags.featured) {
-          url = `${apiUrl}&featured=${flags.featured}`;
-        } else if (flags.collection) {
-          url = `${apiUrl}&collection=${flags.collection}`;
-        } else {
-          url = `${apiUrl}`;
-        }
+      if (frun()) {
+        config.set('counter', 0);
+        config.set('archivments', [
+          {
+            name: 'First Download',
+            downloads: 1
+          }, {
+            name: 'I love photos',
+            downloads: 10
+          }, {
+            name: 'HyperDownloader',
+            downloads: 100
+          }
+        ]);
 
-        splash(url, photo => {
-          download({
-            filename: join(config.get('pic_dir'), `${photo.id}.jpg`),
-            photo
-          }, flags);
+        figlet('Splash!', (err, data) => {
+            if (err) {
+                console.log('Something went wrong...');
+                console.dir(err);
+                return;
+            }
+            console.log(data)
+            console.log();
+
+            mkdirp(pathParse('~/Pictures/splash_photos'), () =>  {
+              config.set('pic_dir', pathParse('~/Pictures/splash_photos'));
+              console.log(`All photos will stored in: ${config.get('pic_dir')}`)
+              console.log();
+              console.log();
+            })
         });
-      } else {
-        log(`${chalk.yellow('Splash:')} I need an internet connection!`);
-        console.log();
-        console.log(`Proxy Status: ${config.get('proxy')}`);
-        console.log();
-        console.log(`If you are under a ${chalk.bold('proxy')} server, check who '--offline' option is on true.`);
-        process.exit();
       }
-    });
+
+      switch (command) {
+        case 'update':
+          updateCMD();
+          break;
+        case 'dir':
+          dirCMD(flags);
+          break;
+        case 'save':
+          saveCMD(flags);
+          break;
+        case 'list':
+          listCMD(flags);
+          break;
+        case 'restore':
+          restoreCMD();
+          break;
+        case 'clean':
+          cleanCMD();
+          break;
+        case 'archivments':
+          console.log();
+          const archivments = config.get('archivments');
+
+          archivments.forEach(archivment => {
+            console.log(`${chalk.green(archivment.name)} unlock at ${chalk.yellow(archivment.downloads + '↓')}`);
+          })
+          console.log();
+          console.log(`Total Downloads: ${chalk.red(config.get('counter'))}`);
+
+          console.log();
+          break;
+        default:
+          console.log();
+          console.log(`Unknown command "${chalk.cyan(command)}".`);
+          console.log();
+          console.log(cli.help);
+          break;
+      }
+    } else if (flags.id) {
+      idCMD(flags);
+    } else if (flags.size) {
+      sizeCMD(flags);
+    } else {
+      let url = '';
+
+      if (flags.user) {
+        url = `${apiURL}&username=${flags.user}`;
+      } else if (flags.featured) {
+        url = `${apiURL}&featured=${flags.featured}`;
+      } else if (flags.collection) {
+        url = `${apiURL}&collection=${flags.collection}`;
+      } else {
+        url = `${apiURL}`;
+      }
+
+      splash(url, photo => {
+        download({
+          filename: join(config.get('pic_dir'), `${photo.id}.jpg`),
+          photo
+        }, flags, () => {
+          console.log('x');
+        });
+      })
+    }
   }
 }
 
-// Call the main function
+
+
 sp(cli.input[0], cli.flags);
