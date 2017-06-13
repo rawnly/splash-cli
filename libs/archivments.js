ff4ba464f6f3835ef9db0cdaad7cb3617bbb3ea5
Rawnly <fedevitale99@gmail.com>
2017-06-13 23:08:11 +0200

    libs/core.js

---
+++ libs/archivments.js
@@ -0,0 +1,11 @@
+module.exports = (list, counter) => {
+  let unlocked = false;
+
+  list.forEach(archivment => {
+    if ( counter == archivment.downloads ) {
+      unlocked = archivment;
+    }
+  })
+
+  return unlocked;
+}
