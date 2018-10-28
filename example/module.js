f959641b21362b1129cdb0254b6aee4f1dade02f
Rawnly <fedevitale99@gmail.com>
2018-10-28 17:49:14 +0100

    fixed code-style

--- example/module.js
+++ example/module.js
@@ -1,32 +1,32 @@
-const splash = require("../build/client").default
+const splash = require('../build/client').default;
 
 async function newWallpaper(collection = 'editorial') {
-    const options = {};
+	const options = {};
 
-    if (collection) {
-        options.collection = collection
-    } else {
-        options.day = true
-    }
+	if (collection) {
+		options.collection = collection;
+	} else {
+		options.day = true;
+	}
     
-    if ( process.platform == 'darwin' ) {
-        options.screen = 'all'
-    }
+	if ( process.platform == 'darwin' ) {
+		options.screen = 'all';
+	}
 
-    await splash([], options);
+	await splash([], options);
 }
 
 (async () => {
-    await newWallpaper();
-    console.log(`\nWating...\n`)
+	await newWallpaper();
+	console.log('\nWating...\n');
 
-    var interval = 1000 * 10
+	var interval = 1000 * 10;
 
-    setInterval(() => {
-        interval -= 1000
+	setInterval(() => {
+		interval -= 1000;
 
-        process.stdout.write(`Remeaining: ${Math.round(interval / 1000)}s\r`)
-    }, 1000)
+		process.stdout.write(`Remeaining: ${Math.round(interval / 1000)}s\r`);
+	}, 1000);
 
-    setTimeout(async () => await newWallpaper(), 1000 * 10)
-})()
+	setTimeout(async () => await newWallpaper(), 1000 * 10);
+})();
