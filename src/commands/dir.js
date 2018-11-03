13865b49e60f42a564368059933d26632f70884f
Rawnly <fedevitale99@gmail.com>
2018-11-03 14:52:44 +0100

    Improved UX

--- src/commands/dir.js
+++ src/commands/dir.js
@@ -1,9 +1,10 @@
-import printBlock from '@splash-cli/print-block';
-import Directory from './utils/Directory';
-
 import chalk from 'chalk';
 
-export default async function ([cmd]) {
+import Directory from './libs/Directory';
+import { printBlock } from '../extra/utils';
+
+
+export default async function dirCMD([cmd]) {
 	switch (cmd) {
 	case 'clean':
 		return await Directory.clean();
@@ -11,7 +12,20 @@ export default async function ([cmd]) {
 		return printBlock(chalk `That's the path: "{yellow {underline ${Directory.getPath()}}}"`);
 	case 'count':
 		return printBlock(chalk `Found {cyan ${await Directory.count()}} photos.`);
+	case 'help':
+	case 'h':
+	case 'hwo':
+		printBlock('DIR HELP', '', chalk `
+				{bold {black {bgWhite COMMANDS}}}   	{bold {black {bgYellow ALIASES}}} 		{bold {black {bgWhite DESCRIPTION}}}
+
+				{cyan {bold get}}		{dim none}			  {dim GET DOWNLOADED PHOTOS LOCATION}
+				{cyan {bold count}} 	{dim none}			  {dim COUNTS ALL THE DOWNLOADED PHOTOS}
+				{cyan {bold clean}} 	{dim none}	  {dim DELETE ALL THE DOWNLOADED PHOTOS}
+				{cyan {bold help}} 		{yellow "how"}			  {dim SHOWS THIS MESSAGE}
+			`.split('\n').map(item => `  ${item.trim()}`).join('\n'));
+		break;
 	default:
+		if (!cmd) return dirCMD(['help']);
 		printBlock(chalk `Unknown command "{red {bold ${cmd}}}"`);
 	}
 }
