a73823ce30e881b096f0db1a17ab2a305f6dba53
Federico Vitale <fedevitale99@gmail.com>
2019-11-20 23:57:47 +0100

    fixed

--- src/extra/config.js
+++ src/extra/config.js
@@ -28,7 +28,7 @@ export const defaultSettings = {
 export const config = new Conf({ defaults: defaultSettings });
 
 export const keys = {
-	client_id: '',
+	client_id: '891db6b7b57458f71f6d0c54918fa150c269dec61e6cfd3ce51a0d74e89e6df2',
 	client_secret: 'c415049c76eed5e2d072b49191be46d78f27609f4e273a64e37e0f7b2324d9ad',
 	redirect_uri: 'http://localhost:5835/',
 };
