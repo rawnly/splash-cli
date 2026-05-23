1a0ad1e5e68e17674c6a56289446cb033775eb8f
Federico <16429579+rawnly@users.noreply.github.com>
2026-05-23 23:33:38 +0200

    Fix regular photo URL decoding

--- unsplash/models/photo.go
+++ unsplash/models/photo.go
@@ -19,7 +19,7 @@ type Photo struct {
 type PhotoUrls struct {
 	Raw     string `json:"raw"`
 	Full    string `json:"full"`
-	Regular string `json:""`
+	Regular string `json:"regular"`
 	Small   string `json:"small"`
 	Thumb   string `json:"thumb"`
 }
